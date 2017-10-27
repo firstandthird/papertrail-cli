fa49433121adae9885c80cae886fb7c391ebd50e
orthagonal <orthagonal1@gmail.com>
2017-10-27 13:49:34 -0500

    update

--- papertrail-cli.js
+++ papertrail-cli.js
@@ -29,29 +29,33 @@ const argv = require('yargs')
 
 const token = argv.t || argv.token;
 const search = argv._[0];
-const host = `https://papertrailapp.com/api/v1/events/search.json?$q={search}`;
+const host = `https://papertrailapp.com/api/v1/events/search.json?$q=${search}`;
 const delayInMs = argv.delay * 1000;
+let lastTimeQueried;
 
 const printEvent = (event) => {
   const message = argv.o ? event.message.match(new RegExp(argv.o)) : event.message;
   const timestamp = argv.timestamp ? event.generated_at : '';
   const system = (argv.system || argv.s) ? event.system : '';
   const program = (argv.program || argv.p) ? event.program : '';
+  const array = [timestamp, system, program, message].join(' ');
   if (message) {
-    console.log(`${timestamp} ${system} ${program} ${message}`);
+    console.log(array);
   }
 };
 
 const execute = () => {
-  wreck.get(`${host}`, {
-    headers: {
+  const url = lastTimeQueried ? `${host}&min_time=${lastTimeQueried}` : host;
+  wreck.get(url, { headers: {
       'X-Papertrail-Token': token
     },
     json: true
   }, (err, res, payload) => {
     payload.events.forEach(printEvent);
+    if (payload.events.length !== 0) {
+      lastTimeQueried = new Date(payload.events[payload.events.length - 1].generated_at).getTime() / 1000;
+    }
     setTimeout(execute, delayInMs);
   });
 };
-
 execute();
